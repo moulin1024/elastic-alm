2c37cef49eebbcde34f1b2e3bf3447f63c03b645
moulin <moulin1024>
2021-10-28 21:55:06 +0200

    vacuum test

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 cd src
 make clean && make && ./main
 cd ..
+cd post
 python post.py
--- src/parameters.f90
+++ src/parameters.f90
@@ -3,12 +3,12 @@ module parameters
     integer, parameter :: N_start = 2
     integer, parameter :: N_on_blade = N_total - N_start + 1
     integer, parameter :: N = N_on_blade - 2    
-    integer, parameter :: T = 5e5
-    integer, parameter :: t_count = 5e2
+    integer, parameter :: T = 5e6
+    integer, parameter :: t_count = 5e3
     integer, parameter :: simulation_type = 1
     integer, parameter :: blade_num = 2
     real, parameter :: radius = 5
-    real, parameter :: omega = 72/60*2*3.1415926
+    real, parameter :: omega = 72.0/60*2*3.1415926
     real, parameter :: pitch = 3.0/180*3.1415926
     real, parameter :: total_time = 10
     real, parameter :: r_start = 0.508
